2ab6d011ba745176853a3284e8693d5d528d84a6
Florian Champain <f.champain@gmail.com>
2024-07-25 22:11:37 +0200

    migration to C++

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "esp_system.h"
-#include "esp_log.h"
-#include "esp_wifi.h"
-#include "esp_event.h"
-#include "nvs_flash.h"
-#include "driver/gpio.h"
-#include "esp_timer.h"
-#include "esp_http_server.h"
-#include "esp_netif.h"
-#include "cJSON.h"
-#include "esp_adc/adc_continuous.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/semphr.h>
+#include <esp_system.h>
+#include <esp_log.h>
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
+#include <driver/gpio.h>
+#include <esp_timer.h>
+#include <esp_http_server.h>
+#include <esp_netif.h>
+#include <cJSON.h>
+#include <esp_adc/adc_continuous.h>
 
 #define WIFI_SSID "Livebox-Florelie"
 #define WIFI_PASS "r24hpkr2"
@@ -35,14 +35,11 @@ static int64_t maxTime = 0;
 static int64_t minTime = 1000000;
 static int64_t meanTime = 0;
 static const int64_t nbSamples = 1000;
-static volatile int s = 0;
+static int s = 0;
 
 static bool IRAM_ATTR adc_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data) {
     BaseType_t high_task_awoken = pdFALSE;
-    
-    // Signal that new data is ready
-    vTaskNotifyGiveFromISR(user_data, &high_task_awoken);
-
+    vTaskNotifyGiveFromISR(static_cast<TaskHandle_t>(user_data), &high_task_awoken);
     return high_task_awoken == pdTRUE;
 }
 
@@ -52,9 +49,7 @@ static void adc_timer_callback(void* arg) {
    
     for (int i = 0; i < NUM_CHANNELS; i++) {
         xSemaphoreTakeFromISR(mutex, &high_task_awoken);
-        // Pour le format TYPE2, chaque échantillon est sur 4 octets
         uint32_t adc_reading = ((uint32_t*)adc_raw)[i];
-        // Les 12 bits de poids faible contiennent la valeur ADC
         adcValues[i] = adc_reading & 0xFFF;
         xSemaphoreGiveFromISR(mutex, &high_task_awoken);
     }
@@ -91,11 +86,10 @@ static void adc_task(void *pvParameters) {
     };
     ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &adc_handle));
 
-    adc_continuous_config_t dig_cfg = {
-        .sample_freq_hz = SAMPLE_RATE * NUM_CHANNELS,
-        .conv_mode = ADC_CONV_SINGLE_UNIT_1,
-        .format = ADC_DIGI_OUTPUT_FORMAT_TYPE2,
-    };
+    adc_continuous_config_t dig_cfg;
+    dig_cfg.sample_freq_hz = SAMPLE_RATE * NUM_CHANNELS;
+    dig_cfg.conv_mode = ADC_CONV_SINGLE_UNIT_1;
+    dig_cfg.format = ADC_DIGI_OUTPUT_FORMAT_TYPE2;
 
     adc_digi_pattern_config_t adc_pattern[NUM_CHANNELS];
     for (int i = 0; i < NUM_CHANNELS; i++) {
@@ -108,27 +102,25 @@ static void adc_task(void *pvParameters) {
     dig_cfg.adc_pattern = adc_pattern;
     ESP_ERROR_CHECK(adc_continuous_config(adc_handle, &dig_cfg));
 
-    adc_raw = heap_caps_malloc(DMA_BUFFER_SIZE, MALLOC_CAP_DMA);
+    adc_raw = static_cast<uint8_t*>(heap_caps_malloc(DMA_BUFFER_SIZE, MALLOC_CAP_DMA));
     assert(adc_raw != NULL);
 
-    adc_continuous_evt_cbs_t cbs = {
-        .on_conv_done = adc_conv_done_cb,
-    };
+    adc_continuous_evt_cbs_t cbs;
+    cbs.on_conv_done = adc_conv_done_cb;
+
     ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(adc_handle, &cbs, xTaskGetCurrentTaskHandle()));
     
     ESP_ERROR_CHECK(adc_continuous_start(adc_handle));
 
-    esp_timer_create_args_t timer_args = {
-        .callback = &adc_timer_callback,
-        .name = "adc_timer"
-    };
+    esp_timer_create_args_t timer_args;
+    timer_args.callback = &adc_timer_callback;
+    timer_args.name = "adc_timer";
    
     esp_timer_handle_t timer;
     ESP_ERROR_CHECK(esp_timer_create(&timer_args, &timer));
     ESP_ERROR_CHECK(esp_timer_start_periodic(timer, 1000000 / SAMPLE_RATE));
    
     while(1) {
-        // Wait for the conversion to be done
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
         uint32_t ret_num = 0;
@@ -137,13 +129,11 @@ static void adc_task(void *pvParameters) {
             // Process data here if needed
         }
         
-        vTaskDelay(1);  // Give other tasks a chance to run
+        vTaskDelay(1);
     }
 }
 
-
-
-static esp_err_t get_adc_data_handler(httpd_req_t *req) {
+/*static esp_err_t get_adc_data_handler(httpd_req_t *req) {
     cJSON *root = cJSON_CreateObject();
     
     xSemaphoreTake(mutex, portMAX_DELAY);
@@ -196,9 +186,9 @@ static httpd_handle_t start_webserver(void) {
     }
     
     return server;
-}
+}*/
 
-static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+/*static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                int32_t event_id, void* event_data) {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         esp_wifi_connect();
@@ -209,9 +199,9 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
         ESP_LOGI("WiFi", "Got IP: " IPSTR, IP2STR(&event->ip_info.ip));
         start_webserver();
     }
-}
+}*/
 
-static void wifi_init_sta(void) {
+/*static void wifi_init_sta(void) {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_sta();
@@ -222,27 +212,23 @@ static void wifi_init_sta(void) {
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
     ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL, NULL));
     
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = WIFI_SSID,
-            .password = WIFI_PASS,
-            .scan_method = WIFI_FAST_SCAN,
-            .bssid_set = false,
-            .channel = 0,
-            .listen_interval = 0,
-            .sort_method = WIFI_CONNECT_AP_BY_SIGNAL,
-            .threshold = {
-                .authmode = WIFI_AUTH_WPA2_PSK,
-            },
-            .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
-        },
-    };
+    wifi_config_t wifi_config;
+    wifi_config.sta.ssid = WIFI_SSID;
+    wifi_config.sta.password = WIFI_PASS;
+    wifi_config.sta.scan_method = WIFI_FAST_SCAN;
+    wifi_config.sta.bssid_set = false;
+    wifi_config.sta.channel = 0;
+    wifi_config.sta.listen_interval = 0;
+    wifi_config.sta.sort_method = WIFI_CONNECT_AP_BY_SIGNAL;
+    wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+    wifi_config.sta.sae_pwe_h2e = WPA3_SAE_PWE_BOTH;
+
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
-}
+}*/
 
-void app_main(void) {
+extern "C" void app_main(void) {
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
