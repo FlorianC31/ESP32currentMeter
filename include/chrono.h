47584269c97b353bd28abbba1ef7fb93cf100acd
Florian Champain <f.champain@gmail.com>
2024-10-21 07:37:48 +0200

    refactor of chrono class (WIP2)

--- include/chrono.h
+++ include/chrono.h
@@ -18,39 +18,37 @@ public:
 private:
 
     struct Data{
-        std::atomic<int> max = 0;
-        std::atomic<int> min = 1000000;
-        std::atomic<int> mean = 0;
-        std::atomic<int> nbOverDeadline = 0;
-        int deadline;
+        float max = 0.;
+        float min = 1000000.;
+        float mean = 0.;
+        int nbOverDeadline = 0;
+        std::atomic<float> globalMax = 0.;
+        std::atomic<float> globalMin = 1000000.;
+        std::atomic<float> globalMean = 0.;
+        std::atomic<float> globalNbOverDeadline = 0;
+        float limit = 0.;
     };
 
     void print();
     void init();
     void calcGlobal();
+    void calc(Data* data, float value);
 
     std::string m_name;
-
-    Data m_freq;
-    Data m_duration;
-    float m_cpuUsage;
-
 
     int m_limit;
     bool m_debug;
     int m_printFreq;
     int m_iter = 0;
-    int m_maxTime = 0;
-    int m_minTime = 1000000;
-    int m_meanTime = 0;
+    double m_totalIter = 0.;
     int m_startTime;
     int m_lastStartTime;
+    float m_curentFreq = 0.;
+    
+    Data m_freq;
+    Data m_duration;
+    Data m_cpuUsage;
 
-    std::atomic<int> m_totalIter = 0;
-    std::atomic<int> m_totalMaxTime = 0;
-    std::atomic<int> m_totalMinTime = 1000000;
-    std::atomic<int> m_totalMeanTime = 0;
-    std::atomic<int> m_nbOverLimit = 0;
 };
 
 #endif  // __CHRONO_H
--- src/chrono.cpp
+++ src/chrono.cpp
@@ -21,11 +21,9 @@ Chrono::Chrono(std::string name, int limit, int printFreq, bool debug) :
     m_debug(debug),
     m_printFreq(printFreq),
     m_startTime(0),
-    m_nbOverLimit(0)
+    m_lastStartTime(0)
 {
-    ESP_LOGI("Chrono", "Start of constructor");
     init();
-    ESP_LOGI("Chrono", "End of constructor");
 }
 
 
@@ -40,9 +38,17 @@ Chrono::~Chrono()
 void Chrono::init()
 {
     m_iter = 0;
-    m_maxTime = 0;
-    m_minTime = 1000000;
-    m_meanTime = 0;
+    m_freq.max = 0;
+    m_freq.min = 1000000;
+    m_freq.mean = 0;
+
+    m_duration.max = 0;
+    m_duration.min = 1000000;
+    m_duration.mean = 0;
+    
+    m_cpuUsage.max = 0;
+    m_cpuUsage.min = 1000000;
+    m_cpuUsage.mean = 0;
 }
 
 /**
@@ -53,7 +59,17 @@ void Chrono::init()
 void Chrono::startCycle()
 {
     m_startTime = esp_timer_get_time();
+
+    if (m_lastStartTime == 0) {
+        return;
+    }
+
+    int end_time = esp_timer_get_time();    // µs
+    m_curentFreq = 1000000 / end_time;          // Hz
+    calc(&m_freq, m_curentFreq);
 }
+
+
 
 /**
  * @brief Ends a timing cycle.
@@ -70,16 +86,11 @@ void Chrono::endCycle()
     int end_time = esp_timer_get_time();
 
     int adc_conversion_time = end_time - m_startTime;
-    if (adc_conversion_time > m_maxTime) {
-        m_maxTime = adc_conversion_time;
-    }
-    if (adc_conversion_time < m_minTime) {
-        m_minTime = adc_conversion_time;
-    }
-    if (adc_conversion_time > m_limit) {
-        m_nbOverLimit++;
-    }
-    m_meanTime = (m_meanTime * m_iter + adc_conversion_time) / (m_iter + 1);
+    calc(&m_duration, static_cast<float>(adc_conversion_time));
+
+    float cpuUsage = adc_conversion_time * m_curentFreq / 10000.;
+    calc(&m_cpuUsage, cpuUsage);
+
     m_iter++;
     if (m_iter == m_printFreq) {
         calcGlobal();
@@ -90,6 +101,22 @@ void Chrono::endCycle()
     }
 }
 
+
+
+void Chrono::calc(Data* data, float value)
+{
+    if (value > data->max) {
+        data->max = value;
+    }
+    if (value < data->min) {
+        data->min = value;
+    }
+    if (value > data->limit) {
+        data->nbOverDeadline++;
+    }
+    data->mean = (data->mean * m_iter + value) / (m_iter + 1);
+}
+
 /**
  * @brief Calcul the global timing statistics (for API GET)
  *
@@ -97,13 +124,31 @@ void Chrono::endCycle()
  */
 void Chrono::calcGlobal()
 {
-    if (m_maxTime > m_totalMaxTime) {
-        m_totalMaxTime = m_maxTime;
+    if (m_duration.max > m_duration.globalMax) {
+        m_duration.globalMax = m_duration.max;
     }
-    if (m_minTime < m_totalMinTime) {
-        m_totalMinTime = m_minTime;
+    if (m_duration.min < m_duration.globalMin) {
+        m_duration.globalMin = m_duration.min;
     }
-    m_totalMeanTime = (m_totalMeanTime * m_totalIter + m_meanTime) / (m_totalIter + 1);
+    m_duration.globalMean = (m_duration.globalMean * m_totalIter + m_duration.mean) / (m_totalIter + 1);
+
+    
+    if (m_freq.max > m_freq.globalMax) {
+        m_freq.globalMax = m_freq.max;
+    }
+    if (m_freq.min < m_freq.globalMin) {
+        m_freq.globalMin = m_freq.min;
+    }
+    m_freq.globalMean = (m_freq.globalMean * m_totalIter + m_freq.mean) / (m_totalIter + 1);
+
+    
+    if (m_cpuUsage.max > m_cpuUsage.globalMax) {
+        m_cpuUsage.globalMax = m_cpuUsage.max;
+    }
+    if (m_cpuUsage.min < m_cpuUsage.globalMin) {
+        m_cpuUsage.globalMin = m_cpuUsage.min;
+    }
+    m_cpuUsage.globalMean = (m_cpuUsage.globalMean * m_totalIter + m_cpuUsage.mean) / (m_totalIter + 1);
     m_totalIter++;
 }
 
@@ -114,7 +159,9 @@ void Chrono::calcGlobal()
  */
 void Chrono::print()
 {
-    ESP_LOGI(m_name.c_str(), "min: %iµs - max: %iµs - mean: %iµs", m_minTime, m_maxTime, m_meanTime);
+    ESP_LOGI(m_name.c_str(), "min: %fµs - max: %fµs - mean: %fµs", m_duration.min, m_duration.max, m_duration.mean);
+    ESP_LOGI(m_name.c_str(), "min: %fHz - max: %fHz - mean: %fHZ", m_freq.min, m_freq.max, m_freq.mean);
+    ESP_LOGI(m_name.c_str(), "min: %f%% - max: %f%% - mean: %f%%", m_cpuUsage.min, m_cpuUsage.max, m_cpuUsage.mean);
 }
 
 /**
@@ -129,10 +176,27 @@ std::string Chrono::getGlobalStats()
 
     cJSON_AddStringToObject(json, "Chrono", m_name.c_str());
     cJSON_AddNumberToObject(json, "ElapsedTime(s)", m_totalIter);
-    cJSON_AddNumberToObject(json, "min(µs)", m_totalMinTime);
-    cJSON_AddNumberToObject(json, "mean(µs)", m_totalMeanTime);
-    cJSON_AddNumberToObject(json, "max(µs)", m_totalMaxTime);
-    cJSON_AddNumberToObject(json, std::string("nbOver" + std::to_string(m_limit) +"µs(%)").c_str(), (float)m_nbOverLimit / (m_totalIter * m_printFreq) * 100);
+
+    cJSON* jsonFreq = cJSON_CreateObject();
+    cJSON_AddNumberToObject(jsonFreq, "min(Hz)", m_freq.globalMin);
+    cJSON_AddNumberToObject(jsonFreq, "mean(Hz)", m_freq.globalMean);
+    cJSON_AddNumberToObject(jsonFreq, "max(Hz)", m_freq.globalMax);
+    cJSON_AddNumberToObject(jsonFreq, std::string("nbOver " + std::to_string(m_limit) +"µs(%)").c_str(), (float)m_freq.nbOverDeadline / (m_totalIter * m_printFreq) * 100);
+    cJSON_AddItemToObject(json, "Frequency", jsonFreq);
+
+    cJSON* jsonDuration = cJSON_CreateObject();
+    cJSON_AddNumberToObject(jsonDuration, "min(µs)", m_duration.globalMin);
+    cJSON_AddNumberToObject(jsonDuration, "mean(µs)", m_duration.globalMean);
+    cJSON_AddNumberToObject(jsonDuration, "max(µs)", m_duration.globalMax);
+    cJSON_AddNumberToObject(jsonDuration, std::string("nbOver " + std::to_string(m_limit) +"µs(%)").c_str(), (float)m_duration.nbOverDeadline / (m_totalIter * m_printFreq) * 100);
+    cJSON_AddItemToObject(json, "Task Duration", jsonDuration);
+
+    cJSON* jsonCpuUsage = cJSON_CreateObject();
+    cJSON_AddNumberToObject(jsonCpuUsage, "min(%)", m_cpuUsage.globalMin);
+    cJSON_AddNumberToObject(jsonCpuUsage, "mean(%)", m_cpuUsage.globalMean);
+    cJSON_AddNumberToObject(jsonCpuUsage, "max(%)", m_cpuUsage.globalMax);
+    cJSON_AddNumberToObject(jsonCpuUsage, std::string("nbOver " + std::to_string(m_limit) +"µs(%)").c_str(), (float)m_duration.nbOverDeadline / (m_totalIter * m_printFreq) * 100);
+    cJSON_AddItemToObject(json, "CPU Usage", jsonCpuUsage);
 
     std::string globalStats = cJSON_Print(json);
     cJSON_Delete(json); 
