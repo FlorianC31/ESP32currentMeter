bd83d6b0d8f4ee8f16ea921c9c3e3e7cc0df280d
FlorianC31 <f.champain@gmail.com>
2024-10-22 22:04:41 +0200

    New Adc check

--- include/adc.h
+++ include/adc.h
@@ -6,12 +6,12 @@
 #define ADC_BUFFER_SIZE (NB_CHANNELS * NB_SAMPLES * SOC_ADC_DIGI_RESULT_BYTES)
 
 void adc_task(void *pvParameters);
-//void adc_init();
 
 static const std::array<adc_channel_t, NB_CHANNELS> ADC_CHANNELS = {
-    ADC_CHANNEL_0, ADC_CHANNEL_1, ADC_CHANNEL_2, ADC_CHANNEL_3,
-    ADC_CHANNEL_4, ADC_CHANNEL_5, ADC_CHANNEL_6, ADC_CHANNEL_7
-};
+    ADC_CHANNEL_0, ADC_CHANNEL_1, ADC_CHANNEL_3, ADC_CHANNEL_4,
+    ADC_CHANNEL_5, ADC_CHANNEL_6, ADC_CHANNEL_7, ADC_CHANNEL_8
+};  // Keep ADC1_2 (pin3) free for JTAG
+//pinout ref: https://m.media-amazon.com/images/I/71PHTB8JfiL._AC_SL1293_.jpg
 
 
 
--- src/adc.cpp
+++ src/adc.cpp
@@ -5,14 +5,6 @@
 
 
 
-#if CONFIG_IDF_TARGET_ESP32
-static adc_channel_t channel[2] = {ADC_CHANNEL_6, ADC_CHANNEL_7};
-#else
-static adc_channel_t channel[NB_CHANNELS] = {ADC_CHANNEL_0, ADC_CHANNEL_1, ADC_CHANNEL_2, ADC_CHANNEL_3,
-    ADC_CHANNEL_4, ADC_CHANNEL_5, ADC_CHANNEL_6};
-#endif
-
-
 static const char *TAG = "ADC";
 
 TaskHandle_t adc_task_handle = NULL;
@@ -44,8 +36,8 @@ static void continuous_adc_init(adc_continuous_handle_t *out_handle)
     adc_digi_pattern_config_t adc_pattern[SOC_ADC_PATT_LEN_MAX];
     dig_cfg.pattern_num = NB_CHANNELS;
     for (int i = 0; i < NB_CHANNELS; i++) {
-        adc_pattern[i].atten = ADC_ATTEN_DB_0;
-        adc_pattern[i].channel = channel[i] & 0x7;
+        adc_pattern[i].atten = ADC_ATTEN_DB_11;
+        adc_pattern[i].channel = ADC_CHANNELS[i] & 0x7;
         adc_pattern[i].unit = ADC_UNIT_1;
         adc_pattern[i].bit_width = SOC_ADC_DIGI_MAX_BITWIDTH;
 
@@ -76,6 +68,8 @@ void adc_task(void *pvParameters) {
     ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(handle, &cbs, NULL));
     ESP_ERROR_CHECK(adc_continuous_start(handle));
 
+    std::array<uint16_t, NB_CHANNELS> adcData;
+
     while (1) {
 
         /**
@@ -95,14 +89,22 @@ void adc_task(void *pvParameters) {
             //ESP_LOGI("TASK", "ret is %x, ret_num is %"PRIu32" bytes", ret, ret_num);
             for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES) {
                 adc_digi_output_data_t *p = (adc_digi_output_data_t*)&result[i];
-                uint32_t chan_num = p->type2.channel;
-                uint32_t data = p->type2.data;
+                uint16_t chan_num = static_cast<uint16_t>(p->type2.channel);
+                uint16_t data = static_cast<uint16_t>(p->type2.data);
                 /* Check the channel number validation, the data is invalid if the channel num exceed the maximum channel */
                 if (chan_num < SOC_ADC_CHANNEL_NUM(ADC_UNIT_1)) {
-                    //ESP_LOGI(TAG, "Unit: %s, Channel: %"PRIu32", Value: %"PRIx32, unit, chan_num, data);
+                    ESP_LOGI(TAG, "Unit: %s, Channel: %u, Value: %u", "ADC1", chan_num, data);
+                    adcData[chan_num] = data;
                 } else {
-                    ESP_LOGW(TAG, "Invalid data [%s_%lu_%lu]", "ADC1", chan_num, data);
+                    ESP_LOGW(TAG, "Invalid data [%s_%u_%u]", "ADC1", chan_num, data);
                 }
+
+                /*if (xQueueSend(adcDataQueue, &adcData, 1) != pdPASS) {
+                    ESP_LOGE(TAG, "Error sending ADC data to the queue");
+                }
+                else {
+                    //ESP_LOGE(TAG, "ADC data successfully sent to the queue - %i", nbSample);
+                }*/
             }
             /**
              * Because printing is slow, so every time you call `ulTaskNotifyTake`, it will immediately return.
